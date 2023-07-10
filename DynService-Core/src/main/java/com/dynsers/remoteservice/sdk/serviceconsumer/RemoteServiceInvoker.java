b63c037a07873d7fcccd603a99208d332010fe79
SARTORIUS\Jian.Li1 <jian.li1@sartorius.com>
2023-07-10 16:22:57 +0200

    0001 : add license info

--- DynService-Core/src/main/java/com/dynsers/remoteservice/sdk/serviceconsumer/RemoteServiceInvoker.java
+++ DynService-Core/src/main/java/com/dynsers/remoteservice/sdk/serviceconsumer/RemoteServiceInvoker.java
@@ -1,9 +1,24 @@
+/*
+
+Copyright Jian Li, lijianjoin@gmail.com,
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package com.dynsers.remoteservice.sdk.serviceconsumer;
 
 import com.dynsers.remoteservice.sdk.data.RSMethodRequest;
 import com.dynsers.remoteservice.sdk.data.RSMethodResponse;
 import com.dynsers.remoteservice.sdk.data.RemoteServiceId;
-import com.dynsers.remoteservice.sdk.exceptions.RSException;
 import com.dynsers.remoteservice.sdk.exceptions.RSInvocationException;
 import com.dynsers.remoteservice.sdk.exceptions.RSResponseErrorException;
 import com.fasterxml.jackson.core.JsonProcessingException;
