fbf532b09331b06608ec4596960fc0d906a7f288
SARTORIUS\Jian.Li1 <jian.li1@sartorius.com>
2024-06-12 21:20:07 +0200

    lijianjoin - upgrade the framework to version 1.0.0

--- DynService-Core/src/main/java/com/dynsers/core/serviceconsumer/RemoteServiceConsumerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dynsers.core.serviceconsumer;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.dynsers.remoteservice.sdk")
-public class RemoteServiceConsumerConfig {
-}
