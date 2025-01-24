c59dad95ca95711621c8aa497e78e8985020e021
Jian Li <lijianjoin@gmail.com>
2025-01-24 08:44:19 +0100

    Jian - add timestamp

--- DynService-Client-Example/src/test/java/com/dynsers/remoteservice/SpringRemoteFramework/Caller/AppTest.java
+++ DynService-Client-Example/src/test/java/com/dynsers/remoteservice/SpringRemoteFramework/Caller/AppTest.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:DynService-Client-Example/src/test/java/com/dynsers/remoteservice/SpringRemoteFramework/Caller/AppTest.java
 package com.dynsers.remoteservice.SpringRemoteFramework.Caller;
-========
-package com.dynsers.core.SpringRemoteFramework.Caller;
->>>>>>>> master:DynService-Client-Example/src/test/java/com/dynsers/core/SpringRemoteFramework/Caller/AppTest.java
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -11,32 +7,28 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
